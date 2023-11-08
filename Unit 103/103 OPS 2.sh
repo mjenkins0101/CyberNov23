13b475e8fc765b18ca3e2af2f843f1a46aa6e960
mjenkins0101 <marquel.jenkins0101@gmail.com>
2023-11-07 22:15:47 -0500

    103 OPS 2.SH

---
+++ Unit 103/103 OPS 2.sh
@@ -0,0 +1,14 @@
+# #!bin/bash
+# Declare a variable with as your name
+# Print to the screen Welcome to the Class Name(variable)
+
+Name(variable)
+echo "What is your name?"
+
+read name
+
+echo "Welcome to the class $name"
+sleep 1
+echo "How is your day going $name?"
+sleep 1
+echo "I hope you learn something cool today"

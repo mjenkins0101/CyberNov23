eb83427b374f84b3e7eec1083fae65e343daf4ba
mjenkins0101 <marquel.jenkins0101@gmail.com>
2023-11-08 19:25:26 -0500

    103 OPS 3.sh

---
+++ Unit 103/103 OPS 3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Condtinal Statment 
+# This ops challenges is about conditinal stamtment of if else statments and how they work
+# We are going to take a varibale with the number and have the computer tell us if its greater than 5 less than 5 or equals 5
+
+number=
+
+
+
+
+x=7
+
+if [ "$x" -eq "5" ]
+then
+echo "The Number is 5"
+elif [ "$x" -gt "7" ]
+then
+echo "The number is greater than 5"
+else [ "$x" -lt "5" ]
+echo "The number is less than 5"
+fi 

c6ad4c027b7537fa6c1d62f7719fd0a3d72523ef
mjenkins0101 <marquel.jenkins0101@gmail.com>
2023-11-16 20:18:24 -0500

    3ops8.sh

--- Unit 103/3ops8.sh
+++ Unit 103/3ops8.sh
@@ -3,18 +3,8 @@
 #In this script we want to contiune to ping our personal pc in a infitine while loop
 #Somebody that had a control of a bot net could set up this script on thousands of computer and ping sites till they are overloaded and crash
 
-
-from scapy.all import *
-source_IP = input("Enter IP address of Source: ")
-target_IP = input("Enter IP address of Target: ")
-source_port = int(input("Enter Source Port Number:"))
-i = 1
-
-while True:
-   IP1 = IP(source_IP = source_IP, destination = target_IP)
-   TCP1 = TCP(srcport = source_port, dstport = 80)
-   pkt = IP1 / TCP1
-   send(pkt, inter = .001)
-   
-   print ("packet sent ", i)
-      i = i + 1
+x=1
+while [ $x = 1 ]
+do
+    ping 10.0.0.75
+done

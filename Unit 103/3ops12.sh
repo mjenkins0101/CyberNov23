f37db1a07ee2389624202079a98709355d51a8ae
mjenkins0101 <marquel.jenkins0101@gmail.com>
2023-11-28 19:30:37 -0500

    3ops12.SH

--- Unit 103/3ops12.sh
+++ Unit 103/3ops12.sh
@@ -1,13 +1,16 @@
-    if side1==side2==side3:
-        return "Equilateral Triangle"
-    elif side1 == side2 or side1 == side3 or side2 == side3:
-        return "Isosceles Triangle"
-    else:
-        return "Scalene Triangle"
+#!/bin/bash
 
-side1 = int(input("Enter the length of side 1: "))
-side2 = int(input("Enter the length of side 2: "))
-side3 = int(input("Enter the length of side 3: "))
+echo "How is your day today? (Enter 'good' or 'bad'):"
+read response
 
-result = triangle_type(side1, side2, side3)
-print("The triangle is a", result)
+case "$response" in
+  "good")
+    echo "That's great to hear! Keep it up!"
+    ;;
+  "bad")
+    echo "I'm sorry to hear that. Tomorrow is a new day!"
+    ;;
+  *)
+    echo "Invalid response. Please enter 'good' or 'bad'."
+    ;;
+esac
